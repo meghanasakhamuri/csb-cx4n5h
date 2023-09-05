04e2a801ebb4768964882abec211a812c89e5020
Code Sandbox <hello@codesandbox.io>
2023-09-05 19:10:18 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.eab1de34dcddf85be182fc22d5eb51b7.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "072c505534442cf9319da45977581a72",
+    "url": "https://meghanasakhamuri.github.io/csb-cx4n5h/index.html"
+  },
+  {
+    "revision": "333ae1545be604ef8ace",
+    "url": "https://meghanasakhamuri.github.io/csb-cx4n5h/static/js/main.914fe783.chunk.js"
+  },
+  {
+    "revision": "5f4e7513a0a138a5959a",
+    "url": "https://meghanasakhamuri.github.io/csb-cx4n5h/static/js/runtime-main.ee1c9f13.js"
+  }
+]);
